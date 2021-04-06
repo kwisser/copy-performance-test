e9e4164809dfad48eee62dfe687ba1ed601303b6
Klemens <wisserklemens@gmail.com>
2021-04-07 01:35:37 +0200

    Changed copyFile()

--- CopySpeedTest/main.cpp
+++ CopySpeedTest/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <chrono>
 
+// Copy File, ask User for Source and Target Folder, then Copy
+// example:
+// Enter the Name of Source File: /home/klemon/CLionProjects/open_pearl_projektarbeit/testfiles/50mb
+// Enter the Name of Target File: /home/klemon/CLionProjects/open_pearl_projektarbeit/testfiles/50mbcopied
+
 int copyFile(){
     char ch, sourceFile[80], targetFile[80];
     FILE *fs, *ft;
@@ -14,6 +19,8 @@ int copyFile(){
     }
     std::cout << "\nEnter the Name of Target File: ";
     std::cin >> targetFile;
+    // Start Time
+    std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
     ft = fopen(targetFile, "w");
     if(ft == NULL)
     {
@@ -26,6 +33,15 @@ int copyFile(){
         fputc(ch, ft);
         ch = fgetc(fs);
     }
+
+    // Stop Time
+    std::chrono::high_resolution_clock::time_point end = std::chrono::high_resolution_clock::now();
+    // end - start = time needed for copying File from Source to Target
+    std::chrono::high_resolution_clock::duration difference = end - start;
+    // Cast into milliseconds
+    int ms = std::chrono::duration_cast<std::chrono::milliseconds>(difference).count();
+    std::cout << "Test Took: " << ms << "ms." << std::endl;
+
     std::cout << "\nFile copied successfully.";
     fclose(fs);
     fclose(ft);
@@ -34,20 +50,7 @@ int copyFile(){
 }
 
 int main() {
-    // Start Time
-    std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
-    // Copy File, ask User for Source and Target Folder, then Copy
-    // example:
-    // Enter the Name of Source File: /home/klemon/CLionProjects/open_pearl_projektarbeit/testfiles/50mb
-    // Enter the Name of Target File: /home/klemon/CLionProjects/open_pearl_projektarbeit/testfiles/50mbcopied
     copyFile();
-    // Stop Time
-    std::chrono::high_resolution_clock::time_point end = std::chrono::high_resolution_clock::now();
-    // end - start = time needed for copying File from Source to Target
-    std::chrono::high_resolution_clock::duration difference = end - start;
-    // Cast into milliseconds
-    int ms = std::chrono::duration_cast<std::chrono::milliseconds>(difference).count();
-    std::cout << "Test Took: " << ms << "ms." << std::endl;
     return 0;
 }
 
