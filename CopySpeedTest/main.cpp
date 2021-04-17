4dbb55c57b416aff74ef1ddb47c3fec4a129bd82
mudfsf <73954508+Marceldfsf@users.noreply.github.com>
2021-04-17 14:10:52 +0200

    open close

--- CopySpeedTest/main.cpp
+++ CopySpeedTest/main.cpp
@@ -1,70 +1,106 @@
 #include <iostream>
 #include <chrono>
+#include <pthread.h>
 
 // Copy File, ask User for Source and Target Folder, then Copy
 // example:
 // Enter the Name of Source File: /home/klemon/CLionProjects/open_pearl_projektarbeit/testfiles/50mb
 // Enter the Name of Target File: /home/klemon/CLionProjects/open_pearl_projektarbeit/testfiles/50mbcopied
 
-int copyFile(){
-    char ch, sourceFile[80], targetFile[80];
+void* copyFile( void* help){ char ch, sourceFile[80], targetFile[80]; 
     FILE *fs, *ft;
-    std::cout << "Enter the Name of the File you want to Copy: ";
-    std::cin >> sourceFile;
-    fs = fopen(sourceFile, "r");
-    if(fs == NULL)
-    {
-        std::cout<<"\nError Occurred!";
-        return 0;
-    }
-    std::cout << "\nEnter the Name of Target File: ";
-    std::cin >> targetFile;
-    // Start Read Time Counter
-    std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
-    ft = fopen(targetFile, "w");
-    if(ft == NULL)
-    {
-        std::cout<<"\nError Occurred!";
-        return 0;
-    }
-    ch = fgetc(fs);
-
-    // Stop Read Time Time Counter
-    std::chrono::high_resolution_clock::time_point end = std::chrono::high_resolution_clock::now();
-    // end - start = time needed to Read File
-    std::chrono::high_resolution_clock::duration difference = end - start;
-    // Cast into milliseconds
-    int read_time_in_ms = std::chrono::duration_cast<std::chrono::milliseconds>(difference).count();
-
-    // Start Write Time Counter
+    std::chrono::high_resolution_clock::time_point start, end;
+    std::chrono::high_resolution_clock::duration difference;
+    int read, write, open, close;
+    
     start = std::chrono::high_resolution_clock::now();
-
-    while(ch != EOF)
-    {
-        fputc(ch, ft);
-        ch = fgetc(fs);
-    }
-
-    // Stop Write Time Time Counter
+    fs = fopen("/media/marcel/B282A48982A4539F/open_pearl_projektarbeit-main/testfiles/5mb", "r");
     end = std::chrono::high_resolution_clock::now();
-    // end - start = time needed to Read File
+    if(fs == NULL) {
+        std::cout<<"\nError Occurred!"; return 0;
+    }
     difference = end - start;
-    // Cast into milliseconds
-    int write_time_in_ms = std::chrono::duration_cast<std::chrono::milliseconds>(difference).count();
-
-    std::cout << "\nFile copied successfully." << std::endl;
-    std::cout << "Read Time: " << read_time_in_ms << "ms" << std::endl;
-    std::cout << "Write Time: " << write_time_in_ms << "ms" << std::endl;
-    std::cout << "Total Time: " << read_time_in_ms+write_time_in_ms << "ms" << std::endl;
-
+    open = std::chrono::duration_cast<std::chrono::microseconds>(difference).count();
+    
+    
+    start = std::chrono::high_resolution_clock::now();
     fclose(fs);
+    end = std::chrono::high_resolution_clock::now();
+    difference = end - start;
+    close = std::chrono::duration_cast<std::chrono::microseconds>(difference).count();
+    
+    
+     /*std::cout << "Enter the Name of the File you want 
+    to Copy: "; std::cin >> sourceFile;*/ 
+    start = std::chrono::high_resolution_clock::now(); 
+    fs = fopen("/media/marcel/B282A48982A4539F/open_pearl_projektarbeit-main/testfiles/5mb", "r"); 
+    if(fs == NULL) {
+        std::cout<<"\nError Occurred!";
+        return 0;
+    }
+    
+    /*std::cout << "\nEnter the Name of Target File: "; std::cin >> 
+    targetFile;*/
+    // Start Time
+   
+    ch = fgetc(fs); 
+    	while(ch != EOF) { 
+    	ch = fgetc(fs);
+    }
+    fclose(fs);
+    
+    end = std::chrono::high_resolution_clock::now();
+    difference = end - start;
+    read = std::chrono::duration_cast<std::chrono::milliseconds>(difference).count();
+    
+    
+    
+    
+    start = std::chrono::high_resolution_clock::now();
+    
+    fs = fopen("/media/marcel/B282A48982A4539F/open_pearl_projektarbeit-main/testfiles/5mb", "r"); 
+    if(fs == NULL) {
+        std::cout<<"\nError Occurred!";
+        return 0;
+    }
+    
+    ft = fopen("/media/marcel/B282A48982A4539F/open_pearl_projektarbeit-main/testfiles/output", "w");
+    if(ft == NULL) {
+        std::cout<<"\nError Occurred!";
+        return 0;
+    }
+    
+    ch = fgetc(fs); 
+    	while(ch != EOF) { 
+    	fputc(ch, ft);
+    	ch = fgetc(fs);
+    }
+    
+    fclose(fs); 
     fclose(ft);
-    std::cout << std::endl;
-    return 0;
+    end = std::chrono::high_resolution_clock::now();
+    difference = end - start;
+    write = std::chrono::duration_cast<std::chrono::milliseconds>(difference).count();
+     
+    std::cout << "Open Took: " << open << "µs." << std::endl;
+    std::cout << "Close Took: " << close << "µs." << std::endl;
+    std::cout << "Read Took: " << read << "ms." << std::endl;
+    std::cout << "Write Took: " << write << "ms." << std::endl;
+    std::cout << "\nFile copied successfully." << std::endl;
+
+    return nullptr;
 }
 
-int main() {
-    copyFile();
-    return 0;
-}
+int main() { 
+	pthread_t test; int rc;
 
+	copyFile(nullptr);
+	std::cout << "\n\n\nThreads\n" << std::endl;
+	rc = pthread_create(&test, NULL, copyFile, NULL); 
+	if (rc) { 
+        	printf("Error:unable to create thread, %d\n", rc);
+        	exit(-1);
+      	}
+	pthread_join(test, NULL); 
+	return 0;
+}
