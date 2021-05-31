37c41936b9455c07d361c2181ea825b20121d6e5
Klemens <wisserklemens@gmail.com>
2021-05-31 13:26:58 +0200

    Main.cpp und Copy_Test.cc zusammen geführt

---
+++ CopySpeedTest/Main.cc
@@ -0,0 +1,234 @@
+#include <pthread.h>
+#include <unistd.h>
+#include <signal.h>
+#include <cstring>
+#include <stdio.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <limits.h>
+#include <iostream>
+#include <chrono>
+#include <pthread.h>
+#include <string.h>
+
+char FILENAME_5[16] = "testfiles/5mb";
+char FILENAME_10[16] = "testfiles/10mb";
+char FILENAME_50[16] = "testfiles/50mb";
+char FILENAME_100[16] = "testfiles/100mb";
+char COPIEDFILENAME[16] = "copied";
+
+int *workaround;
+int fd;
+int nread = 1;
+int start = 1;
+int nread1 = 1;
+char buf[10];
+char sourcePath[PATH_MAX];
+char targetPath[PATH_MAX];
+
+
+void* copyFileSpeed( void* help){
+    FILE *fileSource, *fileTarget;
+    char ch, sourceFile[80], targetFile[80];
+    std::chrono::high_resolution_clock::time_point start, end;
+    std::chrono::high_resolution_clock::duration difference;
+    int read, write, open, close, writeraw, readraw;
+
+    start = std::chrono::high_resolution_clock::now();
+    fileSource = fopen(sourcePath, "r");
+    end = std::chrono::high_resolution_clock::now();
+    if(fileSource == NULL) {
+        std::cout<<"\nError Occurred!"; return 0;
+    }
+    difference = end - start;
+    open = std::chrono::duration_cast<std::chrono::microseconds>(difference).count();
+
+
+    start = std::chrono::high_resolution_clock::now();
+    fclose(fileSource);
+    end = std::chrono::high_resolution_clock::now();
+    difference = end - start;
+    close = std::chrono::duration_cast<std::chrono::microseconds>(difference).count();
+
+    // Start Time
+    start = std::chrono::high_resolution_clock::now();
+    fileSource = fopen(sourcePath, "r");
+    if(fileSource == NULL) {
+        std::cout<<"\nError Occurred!";
+        return 0;
+    }
+
+    ch = fgetc(fileSource);
+    while(ch != EOF) {
+        ch = fgetc(fileSource);
+    }
+    fclose(fileSource);
+
+    end = std::chrono::high_resolution_clock::now();
+    difference = end - start;
+    read = std::chrono::duration_cast<std::chrono::milliseconds>(difference).count();
+
+    start = std::chrono::high_resolution_clock::now();
+
+    fileSource = fopen(sourcePath, "r");
+    if(fileSource == NULL) {
+        std::cout<<"\nError Occurred!";
+        return 0;
+    }
+
+    fileTarget = fopen(targetPath, "w");
+    if(fileTarget == NULL) {
+        std::cout<<"\nError Occurred!";
+        return 0;
+    }
+
+    ch = fgetc(fileSource);
+    while(ch != EOF) {
+        fputc(ch, fileTarget);
+        ch = fgetc(fileSource);
+    }
+
+    fclose(fileSource);
+    fclose(fileTarget);
+    end = std::chrono::high_resolution_clock::now();
+    difference = end - start;
+    write = std::chrono::duration_cast<std::chrono::milliseconds>(difference).count();
+    readraw = (float)read - (float)open/1000 - (float)close/1000;
+    writeraw = write - read;
+
+    std::cout << "Open Took: " << open << "µs." << std::endl;
+    std::cout << "Close Took: " << close << "µs.\n" << std::endl;
+
+    std::cout << "Read Took: " << read << "ms." << std::endl;
+    std::cout << "Write Took: " << write << "ms.\n" << std::endl;
+
+    std::cout << "Read_raw Took: " << readraw << "ms" << std::endl;
+    std::cout << "Write_raw Took: " << writeraw << "ms" << std::endl;
+
+    std::cout << "\nFile copied successfully." << std::endl;
+
+    return nullptr;
+}
+
+void handler(int s) {
+    printf("\ninterrupt by signal handler\n%d\n", nread);
+    return;
+}
+
+void *help(void *a) {
+    while (start);
+    pthread_kill((pthread_t) a, SIGALRM);
+    return nullptr;
+}
+
+void *test(void *a) {
+    printf("in cont read thread");
+    while (nread1 > 0)nread1 = read(fd, &buf, sizeof(buf));
+    printf("finished");
+    return nullptr;
+}
+
+void *copyFile(void *help) {
+    workaround = &errno;
+    start = 0;
+    while (nread > 0)
+        nread = write(STDOUT_FILENO, &buf, sizeof(buf));
+    return nullptr;
+}
+
+int startCopySpeedTest(){
+
+    // --------------------------------- Copy File Speed Start -----------------------------
+
+    std::cout << "Copy File Speed Test" << std::endl;
+    pthread_t test_thread; int rc_speed;
+    copyFileSpeed(nullptr);
+    std::cout << "\n\n\nThreads\n" << std::endl;
+    rc_speed = pthread_create(&test_thread, NULL, copyFile, NULL);
+    if (rc_speed) {
+        printf("Error:unable to create thread, %d\n", rc_speed);
+        exit(-1);
+    }
+    pthread_join(test_thread, NULL);
+    // ---------------------------------Copy File Speed End -----------------------------
+    return 0;
+}
+
+int main() {
+    int user_file_size;
+    char cwd[PATH_MAX];
+
+    // File Size
+    printf("Choose File Size: 5mb(1) 10mb(2) 50mb(3) 100mb(4)\n");
+    scanf("%d",&user_file_size);
+
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+        printf("Current working dir: %s\n", cwd);
+
+        strncpy(sourcePath, cwd, strlen(cwd)-13);
+        if(user_file_size == 1){
+            strcat(sourcePath, FILENAME_5);
+        }
+        if(user_file_size == 2){
+            strcat(sourcePath, FILENAME_10);
+        }
+        if(user_file_size == 3){
+            strcat(sourcePath, FILENAME_50);
+        }
+        if(user_file_size == 4){
+            strcat(sourcePath, FILENAME_100);
+        }
+
+
+        strcat(targetPath, sourcePath);
+        strcat(targetPath, COPIEDFILENAME);
+
+
+        printf("File to Copy: %s\n", sourcePath);
+        std::cout << "Target Path: " << targetPath << std::endl;
+
+    } else {
+        perror("getcwd() error");
+        return 1;
+    }
+    // File Size end
+
+    fd = open(sourcePath, O_RDONLY);
+
+    pthread_t tt, te, tb;
+    int rc, rd, d;
+
+    struct sigaction act;
+
+    sigemptyset(&act.sa_mask);
+    act.sa_handler = handler;
+    act.sa_flags = 0;
+
+    sigaction(SIGALRM, &act, NULL);
+
+    rd = pthread_create(&te, NULL, test, NULL);
+    if (rd) {
+        printf("Error:unable to create thread, %d\n", rc);
+        _exit(-1);
+    }
+
+    rc = pthread_create(&tt, NULL, copyFile, NULL);
+    if (rc) {
+        printf("Error:unable to create thread, %d\n", rc);
+        _exit(-1);
+    }
+
+    d = pthread_create(&tb, NULL, help, (void *) tt);
+    if (d) {
+        printf("Error:unable to create thread, %d\n", rd);
+        _exit(-1);
+    }
+
+    pthread_join(tt, NULL);
+    printf("\n%d %d\n", nread, *workaround);
+
+    std::cout << std::endl;
+
+    startCopySpeedTest();
+
+}
